4a46ea7c61ef1dd30b0154b0d2caa4964d351aa1
zhaoruiguang <940139936@qq.com>
2024-05-03 15:57:52 +0800

    commit

--- read_flower_server/app.js
+++ read_flower_server/app.js
@@ -2,18 +2,23 @@ var https = require('https');
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// 静态目录，首页
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/web_assets'));
 
 var options = {
-	key: fs.readFileSync('./cert/4680409_www.aireadall.com.key'),
-	cert: fs.readFileSync('./cert/4680409_www.aireadall.com.pem')
+	key: fs.readFileSync('./cert/www.aireadall.com.key'),
+	cert: fs.readFileSync('./cert/www.aireadall.com.pem')
 };
 
 https.createServer(options, app).listen(443, function() {
 	console.log('https server listen at port 443 !');
 })
-// app.listen('80',function(){
-// 	console.log('https server listen at port 80 !');
-// })
 
 // 守护进程，防止请求错误导致进程退出
 process.on('uncaughtException', (reason, p) => {
@@ -53,11 +58,60 @@ app.post('/uploadImg', upload.single('image'), function(req, res, next) {
 	let method = JSON.parse(req.body.businessData).baiduApiName; 
 	let options = {};
 	options["baike_num"] = "1";		// 返回百度百科的结果数
-	// console.log(method)
 	client[method](image,options).then(function(result) {
-		// console.log(JSON.stringify(result))
 		res.send(JSON.stringify(result))
 	}).catch(function(err) {
 		res.send(JSON.stringify(err))
 	});
 });
+
+// 请求 chatgpt
+app.post('/chatgpt/search', function(req, res) {
+	if(!(req.body&&req.body.content)) {
+		res.send({
+			code: 400,
+			msg: '提问信息不能为空'
+		})
+	}
+	var chatReqData = JSON.stringify({
+	    "model": "gpt-3.5-turbo",
+		"messages": req.body.content,
+		"temperature": 0.2,
+		"n": 1, // 生成答案的个数
+		"max_tokens": 2000,
+		"frequency_penalty": 0,
+		"presence_penalty": 0,
+		"stop": [" Human:", " AI:"]
+	});
+	var chatReqOptions = {
+	    // hostname: 'api.openai.com',
+	    hostname: 'openai.wefce.com',
+	    port: 443,
+	    path: '/v1/chat/completions',
+	    method: 'POST',
+	    headers: {
+	        'Content-Type': 'application/json',
+	        'Authorization': 'xxxxx---xxxx',
+	    }
+	};
+	var chatReq = https.request(chatReqOptions, (chatRes) => {
+	    var data = '';
+	    chatRes.on('data', (chunk) => {
+	        data += chunk;
+	    });
+	    chatRes.on('end', () => {
+			var buf = Buffer.from(data, 'UTF-8');
+			res.send({
+				code: 200,
+				data: buf.toString('UTF-8')
+			})
+	    });
+	}).on("error", (err) => {
+		res.send({
+			code: 500,
+			msg: err.message
+		})
+	});
+	chatReq.write(chatReqData);
+	chatReq.end();
+})
